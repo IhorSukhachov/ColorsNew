21045b9886eea273c96f7ce1af2298ae148c2375
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-15 22:10:50 +0200

    generating random colors

--- ColorsNew/ViewController.swift
+++ ColorsNew/ViewController.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController  {
+    var colors: [UIColor] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
-    
+    func createRandomCOlors() {
+        for _ in 0..<50 {
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            
+            let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+            colors.append(color)
+        }
+    }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
